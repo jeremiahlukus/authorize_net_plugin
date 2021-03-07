1bc64d74b837ec8e9d4d748c701f91549e110bdc
Jeremiah-Parrack <jeremiah.parrack@coxautoinc.com>
2021-03-07 16:02:40 -0500

    Jeremiah-Parrack | working

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import AuthorizeNetAccept
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // must be at the top
     GeneratedPluginRegistrant.register(with: self)
     
     let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
@@ -15,28 +16,39 @@ import AuthorizeNetAccept
     
     channel.setMethodCallHandler{(methodCall, result) in
         if(methodCall.method == "authorizeNet"){
-            let handler = AcceptSDKHandler(environment: AcceptSDKEnvironment.ENV_TEST)
+            let argErr = FlutterError(code: "BAD_ARGS", message: "Failed to parse arguments!", details: nil)
+            guard let args = methodCall.arguments as? Dictionary<String, String> else {result(argErr); return }
+            guard let env = args["env"] else { result(argErr); return }
+            guard let card_number = args["card_number"] else { result(argErr); return }
+            guard let expiration_month = args["expiration_month"] else { result(argErr); return }
+            guard let expiration_year = args["expiration_year"] else { result(argErr); return }
+            guard let card_cvv = args["card_cvv"] else { result(argErr); return }
+            guard let zip_code = args["zip_code"] else { result(argErr); return }
+            guard let card_holder_name = args["card_holder_name"] else { result(argErr); return }
+            guard let api_login_id = args["api_login_id"] else { result(argErr); return }
+            guard let client_id = args["client_id"] else { result(argErr); return }
+            
+            var handler = AcceptSDKHandler(environment: AcceptSDKEnvironment.ENV_TEST)
+            if(env == "production"){
+                handler = AcceptSDKHandler(environment: AcceptSDKEnvironment.ENV_LIVE)
+            }
+    
             let request = AcceptSDKRequest()
-                 request.merchantAuthentication.name = "7594xDmRz"
-                 request.merchantAuthentication.clientKey = "34Fg4ta24e5Y6VQ8guqgUKguPLxW7EwqWWd2wSzCjwDUTN65w9SZ2Qk3p95X93cs"
-                 request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber = "370000000000002"
-                 request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = "02"
-                 request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = "2022"
-                 request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = "900"
+                 request.merchantAuthentication.name = api_login_id
+                 request.merchantAuthentication.clientKey = client_id
+                 request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber = card_number
+                 request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = expiration_month
+                 request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = expiration_year
+                 request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = card_cvv
+            request.securePaymentContainerRequest.webCheckOutDataType.token.fullName = card_holder_name
+            request.securePaymentContainerRequest.webCheckOutDataType.token.zip = zip_code
             
             handler!.getTokenWithRequest(request, successHandler: { (inResponse:AcceptSDKTokenResponse) -> () in
                 DispatchQueue.main.async {
-                    print("Token--->%@", inResponse.getOpaqueData().getDataValue())
-                    var output = String(format: "Response: %@\nData Value: %@ \nDescription: %@", inResponse.getMessages().getResultCode(), inResponse.getOpaqueData().getDataValue(), inResponse.getOpaqueData().getDataDescriptor())
-                    output = output + String(format: "\nMessage Code: %@\nMessage Text: %@", inResponse.getMessages().getMessages()[0].getCode(), inResponse.getMessages().getMessages()[0].getText())
-                    result(output)
-
+                    result(inResponse.getOpaqueData().getDataValue())
                 };
             }) { (inError:AcceptSDKErrorResponse) -> () in
-              
-                let output = String(format: "Response:  %@\nError code: %@\nError text:   %@", inError.getMessages().getResultCode(), inError.getMessages().getMessages()[0].getCode(), inError.getMessages().getMessages()[0].getText())
-                print(output)
-                result(output)
+                result(inError.getMessages().getMessages()[0].getText())
             }
         }
     
