bf372d3f5fea1fb9aacd29ee6c6982771e6b12c0
okiring paul <okiringpaul1@gmail.com>
2022-11-01 14:50:42 +0300

    fixed error handling

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<authorize_net_plugin/AuthorizeNetPlugin.h>)
+#import <authorize_net_plugin/AuthorizeNetPlugin.h>
+#else
+@import authorize_net_plugin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AuthorizeNetPlugin registerWithRegistrar:[registry registrarForPlugin:@"AuthorizeNetPlugin"]];
+}
+
+@end
